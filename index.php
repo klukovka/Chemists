6d520b6fd3f14101d1c282675848797889077105
klukovka <yuliia.koba@nure.ua>
2021-04-03 23:31:54 +0300

    Create repository

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Головна сторінка</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="col">
+            <div class="row">
+                <div class="container">
+                    <a href="#" class="navbar-brand">APT</a>
+                    <div class="custom-toggler">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar"
+                            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse flex-row-reverse" id="navbar">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Головна<span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item"><a href="#" class="nav-link">Захворювання та лікування</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Оплата і доставка</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Відгуки</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">Контакти</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link">
+                                    <p></p>
+                                    <h2><i class="fas fa-shopping-basket"></i></h2>
+                                    <p>Кошик</p>
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <div class="container">
+                        <form class="form-inline">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Каталог</span>
+                                </div>
+                                <input type="text" placeholder="Пошук..." class="form-control">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-1"></div>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
